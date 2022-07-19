f3bcfdddada9632b232c5bbe3cd17e78f6681b7d
levimake <levimake10@gmail.com>
2022-07-19 06:30:25 +0530

    easy bash scripts

---
+++ install.sh
@@ -0,0 +1,26 @@
+cd dmenu-5.1
+make
+sudo make clean install
+cd ..
+
+cd dwm-6.3
+make
+sudo make clean install
+cd ..
+
+cd slock
+make 
+sudo make clean install
+cd ..
+
+cd slstatus
+make
+sudo make clean install
+cd ..
+
+cd st-0.8.5
+make
+sudo make clean install
+cd ..
+
+exit
---
+++ uninstall.sh
@@ -0,0 +1,21 @@
+cd dmenu-5.1
+sudo make uninstall
+cd ..
+
+cd dwm-6.3
+sudo make uninstall
+cd ..
+
+cd slock
+sudo make uninstall
+cd ..
+
+cd slstatus
+sudo make uninstall
+cd ..
+
+cd st-0.8.5
+sudo make uninstall
+cd ..
+
+exit
